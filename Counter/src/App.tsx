48ae0d2c932540e7d42faa8074eff880c6f53d05
teple <stotr2@gmail.com>
2020-07-07 04:02:24 +0900

    카운터앱(1) - state, props, use 함수

--- Counter/src/App.tsx
+++ Counter/src/App.tsx
@@ -1,78 +1,17 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import styled from 'styled-components/native';
+import Counter from '~/screens/Counter';
 
-const ScrollView = styled.ScrollView`
-  background-color: ${Colors.lighter};
+const Container = styled.View`
+  flex: 1;
+  background-color: #eee;
 `;
 
-const Body = styled.View`
-  background-color: ${Colors.white};
-`;
-
-const SectionContainer = styled.View`
-  margin-top: 32px;
-  padding-horizontal: 24px;
-`;
-
-const SectionDescription = styled.Text`
-  margin-top: 8px;
-  font-size: 18px;
-  font-weight: 400;
-  color: ${Colors.dark};
-`;
-
-const HighLight = styled.Text`
-  font-weight: 700;
-`;
-
-interface Props {}
-
-const App = ({}: Props) => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Header />
-          <Body>
-            <SectionContainer>
-              <SectionDescription>Step One</SectionDescription>
-              <SectionDescription>
-                Edit <HighLight>App.js</HighLight> to change this screen and
-                then come back to see your edits.
-              </SectionDescription>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionDescription>See Your Changes</SectionDescription>
-              <SectionDescription>
-                <ReloadInstructions />
-              </SectionDescription>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionDescription>Debug</SectionDescription>
-              <SectionDescription>
-                <DebugInstructions />
-              </SectionDescription>
-            </SectionContainer>
-            <SectionContainer>
-              <SectionDescription>Learn More</SectionDescription>
-              <SectionDescription>
-                Read the docs to discover what to do next:
-              </SectionDescription>
-            </SectionContainer>
-            <LearnMoreLinks />
-          </Body>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
   );
 };
 
---
+++ Counter/src/screens/Counter/index.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
+import Button from '~/components/Button';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const TitleContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleLabel = styled.Text`
+  font-size: 24px;
+`;
+
+const CountContainer = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CountLabel = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const ButtonContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+interface Props {
+  title?: string;
+  initValue: number;
+}
+
+const Counter = ({title, initValue}: Props) => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <Container>
+      {title && (
+        <TitleContainer>
+          <TitleLabel>{title}</TitleLabel>
+        </TitleContainer>
+      )}
+      <CountContainer>
+        <CountLabel>{initValue + count}</CountLabel>
+      </CountContainer>
+      <ButtonContainer>
+        <Button iconName="plus" onPress={() => setCount(count + 1)} />
+        <Button iconName="minus" onPress={() => setCount(count - 1)} />
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default Counter;
